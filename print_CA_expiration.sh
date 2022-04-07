8656d6e325a2742acb9bba2e69114af82d9e6d72
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-07 16:09:39 +0200

    init

--- print_CA_expiration.sh
+++ print_CA_expiration.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -x
 
-pki_engines=$(curl -s --header "X-Vault-Token: root" http://192.168.1.119:9201/v1/sys/mounts | jq -r '.data | to_entries[] | select(.value.type=="pki") | .key')
+pki_engines=$(curl -s --header "X-Vault-Token: root" \
+	http://192.168.1.119:9201/v1/sys/mounts | \
+	jq -r '.data | to_entries[] | select(.value.type=="pki") | .key')
 
 for engine in $pki_engines; do
-	curl -s http://192.168.1.119:9201/v1/${engine}ca/pem | openssl x509 -enddate -noout;
+	curl -s http://192.168.1.119:9201/v1/${engine}ca/pem | \
+		openssl x509 -enddate -noout;
 done;
 
